8c1cf86dc6e1d876c6cf1c434954c4d43762fb10
Douglas A. C <dac17@inf.ufpr.br>
2021-07-24 19:53:36 -0300

    Adding v2 to the testes. Printing and checking results.

--- trabalho_1/run.sh
+++ trabalho_1/run.sh
@@ -1,13 +1,47 @@
-for i in [1..10]; do  
-    row=100;  
-    col=100; 
-    color=20; 
-    ./accessories/geramapa $row $col $color $seed > /tmp/flood_tmp;
 
-    echo "v1";
-    time ./v1/floodit < /tmp/flood_tmp;         
-    echo "floodit_h1";
-    time ./accessories/floodit_h1 < /tmp/flood_tmp;     
-    echo "floodit_h2";
-    time ./accessories/floodit_h2 < /tmp/flood_tmp;       
-done
+cd v1 && make  && cd ..;
+cd v2 && make  && cd ..;
+ 
+row=100;  
+col=100; 
+color=20; 
+
+./accessories/geramapa $row $col $color $seed > /tmp/flood_tmp;
+
+printf '=%.0s' {1..100};
+printf '\n';
+echo "VERSÃO v1";
+time ./v1/floodit < /tmp/flood_tmp > /tmp/flood_out;
+cat /tmp/flood_out;
+
+cat /tmp/flood_tmp /tmp/flood_out | ./accessories/verifica;
+echo "VERIFICAÇÃO: $?";
+
+printf '=%.0s' {1..100};
+printf '\n';
+
+echo "VERSÃO v2";
+time ./v2/floodit < /tmp/flood_tmp > /tmp/flood_out;  
+cat /tmp/flood_out;
+cat /tmp/flood_tmp /tmp/flood_out | ./accessories/verifica;
+echo "VERIFICAÇÃO: $?";  
+    
+
+printf '=%.0s' {1..100};
+printf '\n';
+echo "VERSÃO floodit_h1";
+time ./accessories/floodit_h1 < /tmp/flood_tmp > /tmp/flood_out;  
+cat /tmp/flood_out;  
+cat /tmp/flood_tmp /tmp/flood_out | ./accessories/verifica;
+echo "VERIFICAÇÃO: $?";
+
+printf '=%.0s' {1..100};
+printf '\n';
+echo "VERSÃO floodit_h2";
+time ./accessories/floodit_h2 < /tmp/flood_tmp > /tmp/flood_out; 
+cat /tmp/flood_out; 
+cat /tmp/flood_tmp /tmp/flood_out | ./accessories/verifica;
+echo "VERIFICAÇÃO: $?";     
+
+cd v1 && make purge  && cd ..;
+cd v2 && make purge  && cd ..;
